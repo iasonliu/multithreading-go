f7ab78ed75548d39936c9b7a8dcfc94e4d1618c3
Xiangxu Liu <xxliu@toughtworks.com>
2021-06-07 13:16:52 +0800

    add hierarchy fix deadlocks

---
+++ deadlocks_train/hierarchy/train.go
@@ -0,0 +1,42 @@
+package hierarchy
+
+import (
+	"sort"
+	"time"
+
+	"github.com/iasonliu/multithreading/deadlocks_train/common"
+)
+
+func lockIntersectionsInDistance(id, reserveStart, reserveEnd int, crossings []*common.Crossing) {
+	var intersectionsToLock []*common.Intersection
+	for _, crossing := range crossings {
+		if reserveEnd >= crossing.Position && reserveStart <= crossing.Position && crossing.Intersection.LockedBy != id {
+			intersectionsToLock = append(intersectionsToLock, crossing.Intersection)
+		}
+	}
+
+	// lock by order
+	sort.Slice(intersectionsToLock, func(i, j int) bool { return intersectionsToLock[i].Id < intersectionsToLock[j].Id })
+	for _, it := range intersectionsToLock {
+		it.Mutex.Lock()
+		it.LockedBy = id
+		time.Sleep(10 * time.Millisecond)
+	}
+}
+
+func MoveTrain(train *common.Train, distance int, crossings []*common.Crossing) {
+	for train.Front < distance {
+		train.Front += 1
+		for _, crossing := range crossings {
+			if train.Front == crossing.Position {
+				lockIntersectionsInDistance(train.Id, crossing.Position, crossing.Position+train.TrainLength, crossings)
+			}
+			back := train.Front - train.TrainLength
+			if back == crossing.Position {
+				crossing.Intersection.LockedBy = -1
+				crossing.Intersection.Mutex.Unlock()
+			}
+		}
+		time.Sleep(30 * time.Millisecond)
+	}
+}
--- deadlocks_train/launcher.go
+++ deadlocks_train/launcher.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/iasonliu/multithreading/deadlocks_train/common"
-	"github.com/iasonliu/multithreading/deadlocks_train/deadlock"
+	"github.com/iasonliu/multithreading/deadlocks_train/hierarchy"
 )
 
 var (
@@ -41,16 +41,16 @@ func main() {
 		intersections[i] = &common.Intersection{Id: i, Mutex: sync.Mutex{}, LockedBy: -1}
 	}
 
-	go deadlock.MoveTrain(trains[0], 300, []*common.Crossing{{Position: 125, Intersection: intersections[0]},
+	go hierarchy.MoveTrain(trains[0], 300, []*common.Crossing{{Position: 125, Intersection: intersections[0]},
 		{Position: 175, Intersection: intersections[1]}})
 
-	go deadlock.MoveTrain(trains[1], 300, []*common.Crossing{{Position: 125, Intersection: intersections[1]},
+	go hierarchy.MoveTrain(trains[1], 300, []*common.Crossing{{Position: 125, Intersection: intersections[1]},
 		{Position: 175, Intersection: intersections[2]}})
 
-	go deadlock.MoveTrain(trains[2], 300, []*common.Crossing{{Position: 125, Intersection: intersections[2]},
+	go hierarchy.MoveTrain(trains[2], 300, []*common.Crossing{{Position: 125, Intersection: intersections[2]},
 		{Position: 175, Intersection: intersections[3]}})
 
-	go deadlock.MoveTrain(trains[3], 300, []*common.Crossing{{Position: 125, Intersection: intersections[3]},
+	go hierarchy.MoveTrain(trains[3], 300, []*common.Crossing{{Position: 125, Intersection: intersections[3]},
 		{Position: 175, Intersection: intersections[0]}})
 
 	ebiten.SetWindowSize(320*3, 320*3)
