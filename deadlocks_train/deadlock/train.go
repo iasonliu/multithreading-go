974aa46dc36ba99beebc0656c7ca0d9220288bef
Xiangxu Liu <xxliu@toughtworks.com>
2021-06-07 10:21:30 +0800

    add deadlocks train

---
+++ deadlocks_train/deadlock/train.go
@@ -0,0 +1,25 @@
+package deadlock
+
+import (
+	"time"
+
+	. "github.com/iasonliu/multithreading/deadlocks_train/common"
+)
+
+func MoveTrain(train *Train, distance int, crossings []*Crossing) {
+	for train.Front < distance {
+		train.Front += 1
+		for _, crossing := range crossings {
+			if train.Front == crossing.Position {
+				crossing.Intersection.Mutex.Lock()
+				crossing.Intersection.LockedBy = train.Id
+			}
+			back := train.Front - train.TrainLength
+			if back == crossing.Position {
+				crossing.Intersection.LockedBy = -1
+				crossing.Intersection.Mutex.Unlock()
+			}
+		}
+		time.Sleep(30 * time.Millisecond)
+	}
+}
